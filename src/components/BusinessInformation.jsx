2a4a39b7e50375c4f68f9f2ea104bcfa553832ca
marcusdev99 <marcus.sea13@gmail.com>
2023-11-29 21:49:47 -0600

    refactor : addition od information to BusinessInformation component

--- src/components/BusinessInformation.jsx
+++ src/components/BusinessInformation.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
+import '../styles/BusinessInformation.css'
+
 export const BusinessInformation = () => {
-  return (
-    <div>
-        
-    </div>
-  )
+    return (
+        <div className='business-information-container'>
+            <div>
+                <h2>MENU</h2>
+            </div>
+            <div className='opening-hours'>
+                <h3>Expresión - Café y Arte</h3>
+                <p>Abierto de 5pm - 11pm</p>
+            </div>
+        </div>
+    )
 }
