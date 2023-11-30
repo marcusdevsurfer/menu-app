7079651745095a956aa031d8ccb2a87336a5ddc4
marcusdev99 <marcus.sea13@gmail.com>
2023-11-29 21:50:00 -0600

    addition of BusinessInformation to menu component

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { BusinessInformation } from './BusinessInformation'
 
 export const Menu = () => {
   return (
     <div>
-        
+        <BusinessInformation/>
     </div>
   )
 }
