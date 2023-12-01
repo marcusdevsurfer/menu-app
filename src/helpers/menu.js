dad9f0e7622e9e9cc60279ff7a7e25406783f403
marcusdev99 <marcus.sea13@gmail.com>
2023-12-01 02:16:45 -0600

    feat: addition of menu object

---
+++ src/helpers/menu.js
@@ -0,0 +1,50 @@
+export const menu = {
+    hotDrinks : [
+        {
+            name : 'Café Americano',
+            price : 50
+        },
+        {
+            name : 'Expresso Americano',
+            price : 60
+        },
+        {
+            name : 'Expresso',
+            price : 60
+        },
+        {
+            name : 'Cappuccino',
+            price : 60
+        },
+        {
+            name : 'Chocolate',
+            price : 50
+        },
+        {
+            name : 'Latte',
+            price : 65
+        },
+        {
+            name : 'Moka',
+            price : 60
+        },
+        {
+            name : 'Chai',
+            price : 60
+        },
+        {
+            name : 'Matcha',
+            price : 60
+        },
+        {
+            name : 'Prensa Francesa',
+            price : 90
+        },
+        {
+            name : 'Te de Manzanilla',
+            price : 25
+        },
+    ],
+    
+ 
+}
