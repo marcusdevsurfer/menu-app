be08983d7af828326f64a24ff16771bf7e9b7b86
marcusdev99 <marcus.sea13@gmail.com>
2023-12-01 14:24:24 -0600

    refact : addition of cold drinks component to app component

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BusinessInformation } from './BusinessInformation'
 import { HotDrinks } from './HotDrinks'
+import { ColdDrinks } from './ColdDrinks'
 
 export const Menu = () => {
   return (
     <div>
         <BusinessInformation/>
         <HotDrinks/>
+        <ColdDrinks/>
     </div>
   )
 }
