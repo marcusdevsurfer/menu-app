fd542802c32b53b250d2b117e446f059a22e9e1c
marcusdev99 <marcus.sea13@gmail.com>
2023-12-01 02:17:12 -0600

    refact: addition of hot drinks component to menu component

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { BusinessInformation } from './BusinessInformation'
+import { HotDrinks } from './HotDrinks'
 
 export const Menu = () => {
   return (
     <div>
         <BusinessInformation/>
+        <HotDrinks/>
     </div>
   )
 }
