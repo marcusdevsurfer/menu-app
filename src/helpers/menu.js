aa220e469f0579ebc8df354410b20c8545361a63
marcusdev99 <marcus.sea13@gmail.com>
2023-12-02 12:44:55 -0600

    minoral changes to menu object

--- src/helpers/menu.js
+++ src/helpers/menu.js
@@ -1,103 +1,132 @@
 export const menu = {
-    hotDrinks : [
+    
+    hotDrinks: [
         {
-            name : 'Café Americano',
-            price : 50
-        },
-        {
-            name : 'Expresso Americano',
-            price : 60
-        },
-        {
-            name : 'Expresso',
-            price : 60
-        },
-        {
-            name : 'Cappuccino',
-            price : 60
-        },
-        {
-            name : 'Chocolate',
-            price : 50
-        },
-        {
-            name : 'Latte',
-            price : 65
-        },
-        {
-            name : 'Moka',
-            price : 60
-        },
-        {
-            name : 'Chai',
-            price : 60
-        },
-        {
-            name : 'Matcha',
-            price : 60
-        },
-        {
-            name : 'Prensa Francesa',
-            price : 90
-        },
-        {
-            name : 'Te de Manzanilla',
-            price : 25
-        },
-    ],
-    coldDrinks : [
-        {
-            name : 'Café',
+            name: 'Café Americano',
             price: 50
         },
         {
-            name : 'Expresso Americano',
+            name: 'Expresso Americano',
             price: 60
         },
         {
-            name : 'Cappuccino',
+            name: 'Expresso',
             price: 60
         },
         {
-            name : 'Late Chocolate Milano',
+            name: 'Cappuccino',
+            price: 60
+        },
+        {
+            name: 'Chocolate',
+            price: 50
+        },
+        {
+            name: 'Latte',
             price: 65
         },
         {
-            name : 'Moka',
+            name: 'Moka',
+            price: 60
+        },
+        {
+            name: 'Chai',
+            price: 60
+        },
+        {
+            name: 'Matcha',
+            price: 60
+        },
+        {
+            name: 'Prensa Francesa',
+            price: 90
+        },
+        {
+            name: 'Te de Manzanilla',
+            price: 25
+        },
+    ],
+
+    coldDrinks: [
+        {
+            name: 'Café',
+            price: 50
+        },
+        {
+            name: 'Expresso Americano',
+            price: 60
+        },
+        {
+            name: 'Cappuccino',
+            price: 60
+        },
+        {
+            name: 'Late Chocolate Milano',
             price: 65
         },
         {
-            name : 'Chai',
+            name: 'Moka',
+            price: 65
+        },
+        {
+            name: 'Chai',
             price: 60
         },
         {
-            name : 'Matcha',
+            name: 'Matcha',
             price: 60
         },
         {
-            name : 'Te Manzanilla',
+            name: 'Te Manzanilla',
             price: 25
         },
         {
-            name : 'Soda Italiana de Maracuya',
+            name: 'Soda Italiana de Maracuya',
             price: 50
         },
         {
-            name : 'Limonada de Fresa',
+            name: 'Limonada de Fresa',
             price: 45
         },
         {
-            name : 'Coca Cola de lata',
+            name: 'Coca Cola de lata',
             price: 30
         },
         {
-            name : 'Agua Mineral',
+            name: 'Agua Mineral',
             price: 30
         },
         {
-            name : 'Agua Natural',
+            name: 'Agua Natural',
             price: 20
         },
-    ]
- 
+    ],
+
+    paninis: {
+
+        description : [
+            'Delicioso pan de masa madre, acompañado de aderezo ranch y chile jalapeño.',
+            'Adicional una ensalada de la casa Lechuga, Cebolla Morada, Jitomate, Pepino. $10'
+        ],
+
+        ingredients : [
+            'Queso Gouda',
+            'Queso Manchego',
+            'Peperoni',
+            'Jamon',
+            'Salami'
+        ],
+        
+        products : [
+            {
+                name: '2 Ingredientes',
+                price: 85
+            },
+            {
+                name: 'Ingrediente extra',
+                price: 10
+            },
+        ]
+}
+
 }
